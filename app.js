a9ab0475ef085a6d2951aa145a13fec6b67e48f2
Long <joshua.ng0605@gmail.com>
2017-12-04 21:37:32 +1300

    change path

--- app.js
+++
@@ -1,9 +0,0 @@
-//toggle About button on Nav
-$(".info").on("click", function(){
-    $(".header-container").slideToggle();
-});
-
-//make bigger project image
-$(".photo-grid-item").on("click", function(){
-    $(".photo-grid-item img").toggleClass("bigger");
-});
