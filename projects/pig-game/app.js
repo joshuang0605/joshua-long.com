f4e7fbf97af62b8a1d8ff99c9d18d282b7eb0661
Joshua Long <joshua.ng0605@gmail.com>
2017-12-08 14:05:52 +1300

    add reset button

--- projects/pig-game/app.js
+++ projects/pig-game/app.js
@@ -9,10 +9,53 @@ GAME RULES:
 
 */
 
-var scores, activePlayer, currentScore, dice
+let scores, activePlayer, roundScore, dice
 scores = [0,0]
 activePlayer = 1;
-dice = Math.floor((Math.random() * 6) + 1);
+roundScore = 0;
 
-var score = document.querySelector('#current-' + activePlayer).textContent = dice;
-var diceImg = document.querySelector('.dice').style.display = 'none';
+//Hide the dice image at the beginning of the game
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+//add eventListener for roll the dice button to display the right dice number image and display its result
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    //1. random number
+    let dice = (Math.floor(Math.random() * 6) + 1)
+        
+    //2. update the dice image
+    //bring back the dice image to BLOCK
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+
+    //3.update the roundScore if the dice is not 1
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //NExt Player and reset score of previous player
+        (activePlayer === 0) ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        //set user score to 0
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        //toggle active player
+        document.querySelector('.player-0-panel').classList.toggle = 'active';
+        document.querySelector('.player-1-panel').classList.toggle = 'active';
+        
+    }
+
+
+
+
+
+
+});
